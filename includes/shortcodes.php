400a8d5971c88f887a8595abd417b8ae9838c22a
Pippin Williamson <pippin@pippinsplugins.com>
2013-01-15 13:07:45 -0600

    Removed trailing whitespace

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,7 +1,7 @@
 <?php
 
 function sc_events_calendar_shortcode( $atts, $content = null ) {
-	
+
 	return '<div id="sc_calendar_wrap">' . sc_get_events_calendar() . '</div>';
 }
 add_shortcode( 'sc_events_calendar', 'sc_events_calendar_shortcode' );

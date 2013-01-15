3bd8fa048c0b0b9420d6f8488f44ac00715a06e2
Pippin Williamson <pippin@pippinsplugins.com>
2013-01-15 13:09:10 -0600

    Fixed problem with months getting loaded as child elements of previous month. CLoses #4

--- assets/js/sc-ajax.js
+++ assets/js/sc-ajax.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 	$('body').on('submit', '.sc_events_form', function() {
 
 		document.body.style.cursor = 'wait';
-		var calendar = $(this).parent().parent().parent().attr('id');
+		var calendar = $(this).parent().parent().parent().parent().attr('id');
 		var data = $(this).serialize();
 	    $.post(sc_vars.ajaxurl, data, function (response) {
 	   		$('#' + calendar).html(response);

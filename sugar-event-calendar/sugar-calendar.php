85d17bc6eab8a400fab4d2ec68d7b882ee7ed5e4
John James Jacoby <johnjamesjacoby@me.com>
2019-11-15 00:22:15 -0600

    Bump versions to 2.0.11.

--- sugar-event-calendar/sugar-calendar.php
+++ sugar-event-calendar/sugar-calendar.php
@@ -144,7 +144,7 @@ final class Plugin {
 
 		// Plugin Version.
 		if ( ! defined( 'SC_PLUGIN_VERSION' ) ) {
-			define( 'SC_PLUGIN_VERSION', '2.0.10' );
+			define( 'SC_PLUGIN_VERSION', '2.0.11' );
 		}
 
 		// Plugin Root File.

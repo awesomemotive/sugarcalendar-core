6477035de0679d44334e33b636fab618870e1a8e
John James Jacoby <johnjamesjacoby@me.com>
2019-12-03 13:09:56 -0600

    Merge Standard into Lite.
    Includes updated Updater approach, allowing for add-ons to better support both Standard and Lite versions.

--- sugar-calendar-lite.php
+++ sugar-calendar-lite.php
@@ -5,7 +5,7 @@
  * Description: A calendar with a sweet disposition.
  * Author:      Sandhills Development, LLC
  * Author URI:  https://sandhillsdev.com
- * Version:     2.0.11
+ * Version:     2.0.12
  * Text Domain: sugar-calendar
  * Domain Path: /sugar-event-calendar/includes/languages/
  */

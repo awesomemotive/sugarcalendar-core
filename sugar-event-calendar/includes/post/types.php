1343990ab75d677efa84dfb052ca2abbaa47e941
John James Jacoby <johnjamesjacoby@me.com>
2020-09-03 16:24:32 -0500

    Add support for new post-type labels.
    WordPress 5.5 added support for menu & admin-bar specific labels. These allow us to change from "Calendar" to "Events" going forward, for better integration with other plugins that use the "name" post-type label in their own pages (Elementor, for example.)

--- sugar-event-calendar/includes/post/types.php
+++ sugar-event-calendar/includes/post/types.php
@@ -56,8 +56,10 @@ function sugar_calendar_register_post_types() {
 
 	// Labels
 	$labels = apply_filters( 'sc_event_labels', array(
-		'name'                     => esc_html_x( 'Calendar', 'post type general name', 'sugar-calendar' ),
+		'name'                     => esc_html_x( 'Events', 'post type general name', 'sugar-calendar' ),
 		'singular_name'            => esc_html_x( 'Event', 'post type singular name', 'sugar-calendar' ),
+		'menu_name'                => esc_html_x( 'Calendar', 'post type menu name', 'sugar-calendar' ),
+		'name_admin_bar'           => esc_html_x( 'Event', 'add new from admin bar', 'sugar-calendar' ),
 		'add_new'                  => esc_html_x( 'Add New', 'event', 'sugar-calendar' ),
 		'add_new_item'             => esc_html__( 'Add New Event', 'sugar-calendar' ),
 		'edit_item'                => esc_html__( 'Edit Event', 'sugar-calendar' ),

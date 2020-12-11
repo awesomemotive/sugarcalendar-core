0dbb7f2ab5387364afa56634ddc46ac565892e64
John James Jacoby <johnjamesjacoby@me.com>
2020-12-11 10:16:07 -0600

    Update docs for new tests.

--- tests/phpunit/testcases/common/tests-time-zones.php
+++ tests/phpunit/testcases/common/tests-time-zones.php
@@ -33,6 +33,8 @@ class TimeZones extends \WP_UnitTestCase {
 
 	/**
 	 * At 2020-11-23 00:00:00, Chicago is 6 hours behind UTC.
+	 *
+	 * @group get_timezone_diff
 	 */
 	public function test_get_timezone_diff_chicago_to_utc() {
 		$assert = sugar_calendar_get_timezone_diff( 'America/Chicago', 'UTC', '2020-11-23 00:00:00' );
@@ -42,6 +44,8 @@ class TimeZones extends \WP_UnitTestCase {
 
 	/**
 	 * At 2020-11-23 00:00:00, Chicago is 1 hour behind New York.
+	 *
+	 * @group get_timezone_diff
 	 */
 	public function test_get_timezone_diff_chicago_to_new_york() {
 		$assert = sugar_calendar_get_timezone_diff( 'America/Chicago', 'America/New_York', '2020-11-23 00:00:00' );
@@ -51,6 +55,8 @@ class TimeZones extends \WP_UnitTestCase {
 
 	/**
 	 * At 2020-11-23 00:00:00, New York is 1 hour ahead of Chicago.
+	 *
+	 * @group get_timezone_diff
 	 */
 	public function test_get_timezone_diff_new_york_to_chicago() {
 		$assert = sugar_calendar_get_timezone_diff( 'America/New_York', 'America/Chicago', '2020-11-23 00:00:00' );
@@ -60,6 +66,8 @@ class TimeZones extends \WP_UnitTestCase {
 
 	/**
 	 * At 2020-11-23 00:00:00, Honalulu is 11 hours behind Midway.
+	 *
+	 * @group get_timezone_diff
 	 */
 	public function test_get_timezone_diff_midway_to_honalulu() {
 		$assert = sugar_calendar_get_timezone_diff( 'Pacific/Midway', 'Pacific/Honalulu', '2020-11-23 00:00:00' );
@@ -70,6 +78,8 @@ class TimeZones extends \WP_UnitTestCase {
 
 	/**
 	 * At 2020-11-23 00:00:00, Cairo is 1 hour behind Moscow.
+	 *
+	 * @group get_timezone_diff
 	 */
 	public function test_get_timezone_diff_cairo_to_moscow() {
 		$assert = sugar_calendar_get_timezone_diff( 'Africa/Cairo', 'Europe/Moscow', '2020-11-23 00:00:00' );
@@ -79,6 +89,8 @@ class TimeZones extends \WP_UnitTestCase {
 
 	/**
 	 * At 2020-11-23 00:00:00, Chicago to New York to UTC is 6 hours.
+	 *
+	 * @group get_timezone_diff_multi
 	 */
 	public function test_get_timezone_diff_multi() {
 
@@ -99,6 +111,8 @@ class TimeZones extends \WP_UnitTestCase {
 
 	/**
 	 * At 2020-11-23 00:00:00, Los Angeles is 3 hours behind New York.
+	 *
+	 * @group get_timezone_diff_multi
 	 */
 	public function test_get_timezone_diff_multi_hours() {
 
@@ -119,6 +133,9 @@ class TimeZones extends \WP_UnitTestCase {
 	}
 
 	/**
+	 * At 2020-12-11 09:20:00, viewing Chicago from New York, New York will be
+	 * ahead of Chicago.
+	 *
 	 * @group get_datetime_object
 	 */
 	public function test_get_datetime_object_chicago_greater_than_new_york() {
@@ -134,6 +151,9 @@ class TimeZones extends \WP_UnitTestCase {
 	}
 
 	/**
+	 * At 2020-12-11 09:20:00, viewing Los Angeles from Chicago, Los Angeles
+	 * will be ahead of Chicago.
+	 *
 	 * @group get_datetime_object
 	 */
 	public function test_get_datetime_object_chicago_greater_than_los_angeles() {
@@ -149,6 +169,8 @@ class TimeZones extends \WP_UnitTestCase {
 	}
 
 	/**
+	 * At 2020-12-11 09:20:00, New York is less than Los Angeles
+	 *
 	 * @group get_datetime_object
 	 */
 	public function test_get_datetime_object_los_angeles_less_than_new_york() {
@@ -159,11 +181,13 @@ class TimeZones extends \WP_UnitTestCase {
 		// 09:20 New York
 		$dto2 = sugar_calendar_get_datetime_object( '2020-12-11 09:20:00', 'America/New_York' );
 
-		// Test that $dto2 is greater than $dto1
+		// Test that $dto2 is less than $dto1
 		$this->assertLessThan( $dto1, $dto2 );
 	}
 
 	/**
+	 * Los Angeles at 2020-12-11 12:20:00 equals New York at 2020-12-11 09:20:00
+	 *
 	 * @group get_datetime_object
 	 */
 	public function test_get_datetime_object_los_angeles_equal_to_new_york() {

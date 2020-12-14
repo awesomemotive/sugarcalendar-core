2a0ffc345e42de39519922e54ed6193feb9146fd
John James Jacoby <johnjamesjacoby@me.com>
2020-12-14 10:46:25 -0600

    Add original HTML to data attribute.
    This aids with troubleshooting text changes.

--- sugar-calendar/includes/themes/legacy/js/sc-time-zones.js
+++ sugar-calendar/includes/themes/legacy/js/sc-time-zones.js
@@ -7,7 +7,7 @@ jQuery( document ).ready( function( $ ) {
 		tz      = Intl.DateTimeFormat().resolvedOptions().timeZone,
 		convert = wp.date.dateI18n;
 
-	// Bail if no time zone
+	// Bail if no browser time zone
 	if ( ! tz.length ) {
 		return;
 	}
@@ -16,17 +16,25 @@ jQuery( document ).ready( function( $ ) {
 	dates.each( function() {
 		var date = $( this ),
 			dt   = date.attr( 'datetime' ),
+			org  = date.html(),
 			html = convert( sc_vars.date_format, dt, tz );
 
-		date.html( html );
+		// Set original to data attribute, and update HTML
+		date
+			.attr( 'data-original', org )
+			.html( html );
 	} );
 
 	// Update time HTML
 	times.each( function() {
 		var time = $( this ),
 			dt   = time.attr( 'datetime' ),
+			org  = time.html(),
 			html = convert( sc_vars.time_format, dt, tz );
 
-		time.html( html );
+		// Set original to data attribute, and update HTML
+		time
+			.attr( 'data-original', org )
+			.html( html );
 	} );
 } );

b7c8fd71e8bb21ed0e0df8834f9773913bf5d18e
John James Jacoby <johnjamesjacoby@me.com>
2021-02-23 12:07:27 -0600

    Posts: introduce sugar_calendar_copy_post().
    This new function copies a post, its taxonomy terms, and its meta-data.

--- sugar-calendar/includes/post/functions.php
+++ sugar-calendar/includes/post/functions.php
@@ -147,3 +147,89 @@ function sugar_calendar_get_event_duration( $post = false ) {
 	// Filter & return
 	return apply_filters( 'sugar_calendar_get_event_duration', $retval, $post, $all_day, $start_date, $end_date );
 }
+
+/**
+ * Copy a post, its taxonomy terms, and all meta data
+ *
+ * @since 2.1.7
+ *
+ * @param int $original_id
+ * @param array $data
+ *
+ * @return int Zero on failure. ID of new post on success.
+ */
+function sugar_calendar_copy_post( $original_id = 0, $data = array() ) {
+
+	// Get the post
+	$post = get_post( $original_id, 'ARRAY_A' );
+
+	// Bail if no original post
+	if ( empty( $post ) ) {
+		return 0;
+	}
+
+	// Set post date timestamp
+	$post['post_date'] = date( 'Y-m-d H:i:s' );
+
+	// Unset some keys, to allow them to be set to their defaults
+	unset(
+		$post['guid'],
+		$post['comment_count'],
+		$post['post_date_gmt']
+	);
+
+	// Allow custom data overrides
+	$save = array_merge( $post, $data );
+
+	// Unset the ID column, so an update does not occur
+	unset( $post['ID'] );
+
+	// Insert the post into the database
+	$new_id = wp_insert_post( $save, false, false );
+
+	// Bail if insert failed
+	if ( empty( $new_id ) ) {
+		return 0;
+	}
+
+	// Get taxonomies for post
+	$taxonomies = get_object_taxonomies( $post['post_type'] );
+
+	// Skip if no taxonomies
+	if ( ! empty( $taxonomies ) ) {
+
+		// Loop through all taxonomies
+		foreach ( $taxonomies as $taxonomy ) {
+
+			// Get the terms for this taxonomy
+			$terms = wp_get_post_terms( $original_id, $taxonomy, array(
+				'fields' => 'names'
+			) );
+
+			// Maybe set terms for this taxonomy
+			if ( ! empty( $terms ) ) {
+				wp_set_object_terms( $new_id, $terms, $taxonomy );
+			}
+		}
+	}
+
+	// Get meta data for post
+	$custom_fields = get_post_custom( $original_id );
+
+	// Skip if no post meta
+	if ( ! empty( $custom_fields ) ) {
+
+		// Loop through all post meta
+		foreach ( $custom_fields as $key => $value ) {
+
+			// Unserialize, to be serialized again when added
+			$value = maybe_unserialize( $value[0] );
+
+			// Add the meta
+			add_post_meta( $new_id, $key, $value );
+		}
+	}
+
+	// Return the ID of the new post ID
+	return $new_id;
+}

142fc7485f8b8ad473775428dd3d13eacf4c28ae
John James Jacoby <johnjamesjacoby@me.com>
2021-06-08 23:38:17 -0500

    Sugar_Calendar\DateTime: Add advance() method.
    This method accepts an array of parameters, and advances itself forwards based on the interval, frequency, and falling back on bymonthdays and bydays.

--- sugar-calendar/includes/classes/objects/class-sc-datetime.php
+++ sugar-calendar/includes/classes/objects/class-sc-datetime.php
@@ -27,25 +27,25 @@ class DateTime extends \DateTime {
 	 *
 	 * @return bool|static
 	 */
-    public static function createFromFormat( $format = '', $time = '', $tzo = null ) {
+	public static function createFromFormat( $format = '', $time = '', $tzo = null ) {
 
 		// Default return value
-        $retval = new static();
+		$retval = new static();
 
 		// Create from parent
-        $parent = parent::createFromFormat( $format, $time, $tzo );
+		$parent = parent::createFromFormat( $format, $time, $tzo );
 
 		// Bail if create failed
-        if ( empty( $parent ) ) {
-            return false;
-        }
+		if ( empty( $parent ) ) {
+			return false;
+		}
 
 		// Set the timestamp
-        $retval->setTimestamp( $parent->getTimestamp() );
+		$retval->setTimestamp( $parent->getTimestamp() );
 
 		// Return
-        return $retval;
-    }
+		return $retval;
+	}
 
 	/**
 	 * Overrides DateTime::format() to add support for day-of-week ordinals.
@@ -115,5 +115,118 @@ class DateTime extends \DateTime {
 
 		// Return
 		return $retval;
+	}
+
+	/**
+	 * Iterate forward, adding a DateInterval.
+	 *
+	 * @since 2.2.0
+	 */
+	public function advance( $args = array() ) {
+
+		// Parse args
+		$r = wp_parse_args( $args, array(
+			'frequency'  => '',
+			'interval'   => '',
+			'bymonthday' => '',
+			'byday'      => ''
+		) );
+
+		// Bail if no recurring frequency
+		if ( empty( $r['frequency'] ) ) {
+			return $this;
+		}
+
+		// Bail if no interval
+		if ( empty( $r['interval'] ) ) {
+			$r['interval'] = 1;
+		}
+
+		// Default to not advancing
+		$advance = false;
+
+		// No negative intervals
+		$interval = abs( $r['interval'] );
+
+		// Adjust by frequency
+		switch ( strtolower( $r['frequency'] ) ) {
+
+			// YEARLY
+			case 'yearly' :
+				$advance = "P{$interval}Y";
+				break;
+
+			// MONTHLY
+			case 'monthly' :
+
+				// Get the Day, Month, and Year
+				$d = $this->format( 'd' );
+				$m = $this->format( 'm' );
+				$y = $this->format( 'Y' );
+
+				// Less than or equal to the 28th day
+				if ( $d <= 28 ) {
+					$advance = "P{$interval}M";
+
+				// Day part is defined by rrule
+				} elseif ( ! empty( $r['bymonthday'] ) || ! empty( $r['byday'] ) ) {
+					$this->setDate( $y, $m + $interval, 28 );
+
+				// Greater than 28 - move day to a safe position
+				} else {
+					$n = 1;
+
+					// Copy for looping
+					$to_loop = clone $this;
+
+					// Set the time to midnight
+					$to_loop->setTime( 0, 0, 0 );
+
+					// Skip subsequent months with less than $d days
+					while ( $d > $to_loop->setDate( $y, ( $m + $interval * $n ), 1 )->format( 't' ) ) {
+						$n++;
+					}
+
+					// Clean up
+					unset( $to_loop );
+
+					// Get return value
+					$advance = "P{$n}M";
+				}
+				break;
+
+			// WEEKLY
+			case 'weekly' :
+				$advance = "P{$interval}W";
+				break;
+
+			// DAILY
+			case 'daily' :
+				$advance = "P{$interval}D";
+				break;
+
+			// HOURLY
+			case 'hourly' :
+				$advance = "PT{$interval}H";
+				break;
+
+			// MINUTELY
+			case 'minutely' :
+				$advance = "PT{$interval}M";
+				break;
+
+			// SECONDLY
+			case 'secondly' :
+				$advance = "PT{$interval}S";
+				break;
+		}
+
+		// Maybe advance
+		if ( ! empty( $advance ) ) {
+			$this->add( new \DateInterval( $advance ) );
+		}
+
+		// Return
+		return $this;
 	}
 }
